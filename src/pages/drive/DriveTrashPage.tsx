0c3da1c0414f7421bdb37ae80c80983b0982c4a8
yunhanoida <yunhano48@gmail.com>
2026-06-04 11:18:58 +0900

    휴지통

---
+++ src/pages/drive/DriveTrashPage.tsx
@@ -0,0 +1,7 @@
+export default function DriveTrashPage() {
+  return (
+    <div className="flex h-full items-center justify-center rounded-xl bg-white">
+      <p className="text-slate-400">휴지통 페이지 구현 중</p>
+    </div>
+  )
+}
